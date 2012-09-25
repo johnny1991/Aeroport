d2b8cbbd3a46aa4e3a857b06b236200488865be4
johnny <johnny.cottereau@gmail.com>
2012-09-25 10:36:20 +0200

    RESET

--- public/index.php
+++ public/index.php
@@ -1,7 +1,4 @@
 <?php
-echo "test valentin";
-echo "test EMERIC";
-echo "test valentin2";
 // Define path to application directory
 defined('APPLICATION_PATH')
     || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
