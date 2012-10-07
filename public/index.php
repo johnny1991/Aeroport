9f15dc962c48d51f6e1af32883077024331d2f38
johnny <johnny.cottereau@gmail.com>
2012-10-08 00:08:26 +0200

    Parametrage des repertoires des modèles, de la librairie. Parametrage des sessions et du loader

--- public/index.php
+++ public/index.php
@@ -7,14 +7,27 @@ defined('APPLICATION_PATH')
 defined('APPLICATION_ENV')
     || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
 
+// Define library directory
+defined ('LIBRARY_PATH') || define('LIBRARY_PATH',
+	realpath(dirname(__FILE__) . '/../library'));
+
+// Define models directory
+defined ('MODELS_PATH') || define('MODELS_PATH',
+		realpath(dirname(__FILE__) . '/models'));
+
 // Ensure library/ is on include_path
-set_include_path(implode(PATH_SEPARATOR, array(
-    realpath(APPLICATION_PATH . '/../library'),
-    get_include_path(),
-)));
+set_include_path(implode(PATH_SEPARATOR, array(realpath('LIBRARY_PATH'),realpath('MODELS_PATH'), get_include_path())));
 
 /** Zend_Application */
 require_once 'Zend/Application.php';
+
+/** Zend_Session */
+require_once 'Zend/Session.php';
+Zend_Session::start();
+
+/** Zend_Loader */
+require_once "Zend/Loader.php";
+Zend_Loader::registerAutoload();
 
 // Create application, bootstrap, and run
 $application = new Zend_Application(
