9581cddd9dfedb2fd3b5a88e52df7555061b9d8f
emeric <breal.emeric@yahoo.fr>
2012-10-22 16:49:18 +0200

    model Ligne

---
+++ application/models/Ligne.php
@@ -0,0 +1,11 @@
+<?php
+class Ligne extends Zend_Db_Table_Abstract
+{
+	protected $_name='ligne';
+	protected $_primary=array('numero_ligne');
+	protected $_referenceMap=array(
+			'id_aeroport_origine'=>array(
+					'columns'=>'id_aeroport',
+					'refTableClass'=>'Aeroport')
+	);
+}
