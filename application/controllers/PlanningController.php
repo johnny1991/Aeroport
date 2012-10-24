4c024a3da5201fc144b32dc9cc3ac1396d71ab3c
Valentin <valentin.thulliez@gmail.com>
2012-10-24 11:07:11 +0200

    CSS de la page listevol et librairie JS

--- application/controllers/PlanningController.php
+++ application/controllers/PlanningController.php
@@ -2,21 +2,63 @@
 class PlanningController extends Zend_Controller_Action
 {
 	public function planningAction(){
-		
+		$this->view->headLink()->appendStylesheet('/css/calendar.jquery.css');
+		$this->view->headScript()->appendFile('/js/PlanningFonction.js');
 	}
 	
 	public function listevolAction(){
-		$timestamp = $this->getParam('date') / 1000;
+		$this->view->headLink()->appendStylesheet('/css/PlanningCSS.css');
 		
-		$NumJour = new Aeroport_Planning(date('N', $timestamp));
-		$currentDay = $NumJour->getTranslateDay();
+		$timestamp = round($this->getParam('date') / 1000);
+		
+		$this->view->timestampNext = $timestamp + (24 * 60 * 60);
+		$this->view->timestampPrec = $timestamp - (24 * 60 * 60);
+		
+		$NumJour = date('N', $timestamp);
+		$NumMois = date('m', $timestamp);
+		
+		$laDateTanslate = new Aeroport_Planning($NumJour, $NumMois);
+		
+		$leJour = $laDateTanslate->getTranslateDay();
+		$leMois = $laDateTanslate->getTranslateMonth();
+		
+		$this->view->laDate = $leJour.' '.date('d', $timestamp).' '.$leMois.' '.date('Y', $timestamp);
+		
+		//Création du tableau contenant tous les vols périodique selon le jour $NumJour;
+		$tabVol = array();
+		
+		$TablePeriodicite = new Periodicite;
+		$TableAeroport = new Aeroport;
+		
+		$periodiciteReq = $TablePeriodicite->select()->from($TablePeriodicite)->where('numero_jour = ?', $NumJour);
+		$periodicites = $TablePeriodicite->fetchAll($periodiciteReq);
+		
+		foreach($periodicites as $periodicite){
+			$ligne = $periodicite->findParentLigne();
+			
+			$aeroport_origine = $TableAeroport->find($ligne->id_aeroport_origine)->current();
+			$ville_origine = $aeroport_origine->findParentVille();
+			$pays_origine = $ville_origine->findParentPays();
+			
+			$aeroport_arrivee = $TableAeroport->find($ligne->id_aeroport_arrivee)->current();
+			$ville_arrivee = $aeroport_arrivee->findParentVille();
+			$pays_arrivee = $ville_arrivee->findParentPays();
+			
+			$tabVol[$ligne->numero_ligne] = array(
+							'pays_origine' => $pays_origine->nom_pays,
+							'aeroport_origine' => $aeroport_origine->id_aeroport,
+							'heure_depart' => $ligne->heure_depart,
+							'pays_arrivee' => $pays_arrivee->nom_pays,
+							'aeroport_arrivee' => $aeroport_arrivee->id_aeroport,
+							'heure_arrivee' => $ligne->heure_arrivee);
+		}
+		
+		$this->view->assign('tabLigne', $tabVol);
 		
 	}
 	
 	public function init(){
 		parent::init();
 		
-		$this->view->headLink()->appendStylesheet('/css/calendar.jquery.css');
-		$this->view->headScript()->offsetSetFile(1000, '/js/calendar.jquery.js');
 	}
 }
--- library/Aeroport/Planning.php
+++ library/Aeroport/Planning.php
@@ -2,13 +2,20 @@
 class Aeroport_Planning {
 	
 	private $_tabJour = array('Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche');
+	private $_tabMois = array('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre');
 	private $_numDay;
+	private $_numMonth;
 	
-	function __construct($numDay){
+	function __construct($numDay, $numMonth){
 		$this->_numDay = $numDay;
+		$this->_numMonth = $numMonth;
 	}
 	
 	function getTranslateDay(){
 		return $this->_tabJour[($this->_numDay-1)];
+	}
+	
+	function getTranslateMonth(){
+		return $this->_tabMois[($this->_numMonth-1)];
 	}
 }
